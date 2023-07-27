fe0103b4e61e0b7056dd9bd56ae9a902edc18e92
ankudinov aleksander <sasha@ankudinov.org.ua>
2023-07-27 18:22:46 +0300

    first version new module

---
+++ iOS_SDK/OneSignalSDK/Source/LAdvertService.h
@@ -0,0 +1,17 @@
+//
+//  LAdvertService.h
+//  OneSignal
+//
+//  Created by ankudinov aleksandr on 27.07.2023.
+//  Copyright © 2023 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LAdvertService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_SDK/OneSignalSDK/Source/LAdvertService.m
@@ -0,0 +1,13 @@
+//
+//  LAdvertService.m
+//  OneSignal
+//
+//  Created by ankudinov aleksandr on 27.07.2023.
+//  Copyright © 2023 Hiptic. All rights reserved.
+//
+
+#import "LAdvertService.h"
+
+@implementation LAdvertService
+
+@end
